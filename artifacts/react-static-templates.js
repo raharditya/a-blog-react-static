78f20989c1a3dacfe76ee38bb0bc3fea2f92db43
Asebodi <aditraharditya45@gmail.com>
2019-09-05 18:03:22 +0700

    Test commit

--- artifacts/react-static-templates.js
+++ artifacts/react-static-templates.js
@@ -5,7 +5,6 @@
 export default {
   '/home/rhdt/Documents/reactTest/a-blog/src/pages/404.js': require('/home/rhdt/Documents/reactTest/a-blog/src/pages/404.js').default,
 '/home/rhdt/Documents/reactTest/a-blog/src/pages/about': require('/home/rhdt/Documents/reactTest/a-blog/src/pages/about').default,
-'/home/rhdt/Documents/reactTest/a-blog/src/pages/blog.js': require('/home/rhdt/Documents/reactTest/a-blog/src/pages/blog.js').default,
 '/home/rhdt/Documents/reactTest/a-blog/src/pages/index.js': require('/home/rhdt/Documents/reactTest/a-blog/src/pages/index.js').default,
 '/home/rhdt/Documents/reactTest/a-blog/src/pages/post.js': require('/home/rhdt/Documents/reactTest/a-blog/src/pages/post.js').default,
 '/home/rhdt/Documents/reactTest/a-blog/src/pages/post': require('/home/rhdt/Documents/reactTest/a-blog/src/pages/post').default
--- src/pages/post.js
+++ src/pages/post.js
@@ -1,32 +1,32 @@
 import React from "react";
-import { withRouteData } from "react-static";
+import { useRouteData } from "react-static";
 import Markdown from "react-markdown";
 import { Link } from "@reach/router";
 
-export default withRouteData(
-  ({ post }) =>
-    console.log(post) || (
-      <React.Fragment>
-        <header>
-          <h1>A Blog</h1>
-          <div className="header-nav">
-            <Link to="/">Home</Link>
-          </div>
-        </header>
+export default () => {
+  const { post } = useRouteData();
+  return (
+    <React.Fragment>
+      <header>
+        <h1>A Blog</h1>
+        <div className="header-nav">
+          <Link to="/">Home</Link>
+        </div>
+      </header>
 
-        <section className="post-header">
-          <h3 className="post-title">{post.title}</h3>
-          <small className="post-date">{post.date}</small>
-        </section>
+      <section className="post-header">
+        <h3 className="post-title">{post.title}</h3>
+        <small className="post-date">5 September 2019</small>
+      </section>
 
-        <article className="post-content">
-          <Markdown source={post.content} />
-        </article>
+      <article className="post-content">
+        {/* <Markdown source={post.content} /> */}
+      </article>
 
-        <footer>
-          <small>raharditya.com</small>
-          <p>Copyright 2019</p>
-        </footer>
-      </React.Fragment>
-    )
-);
+      <footer>
+        <small>raharditya.com</small>
+        <p>Copyright 2019</p>
+      </footer>
+    </React.Fragment>
+  );
+};
